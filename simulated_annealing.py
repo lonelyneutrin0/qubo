265b353beecfcc1d15cacc12afdebdffc5129e11
Pixelqted <pixelatedwastaken@gmail.com>
2024-09-13 00:00:50 -0400

    Logarithmic Annealing Schedule

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -11,26 +11,28 @@ def ob(vec, matrix):
 def prob(old, new, temp): 
     return math.exp(-(new-old)/(temp))
 
-# Parameters
-iterations = 100000
-_ = np.vectorize(lambda x: -100*math.log(x) + math.log(iterations)*100)
-temps = _(np.linspace(1, iterations-1, iterations-1))
-objective_matrix = np.triu(np.random.random((2,2)), k=0)
-vec_i = np.full((objective_matrix.shape[0]),0, dtype=bool)
-vec_i[random.randint(0, vec_i.size-1)] = 1
+def optimize(iterations, objective_matrix): 
+    # Parameters
+    _ = np.vectorize(lambda x: -100*math.log(x) + math.log(iterations)*100)
+    temps = _(np.linspace(1, iterations-1, iterations-1))
 
-# Iteration
-for i in temps:
-    vec_t = vec_i.copy() 
-    
-    # Toggling a random bit
-    random_index = random.randint(0, vec_i.size-1)
-    vec_t[random_index] = not vec_t[random_index]
-    
-    # Avoiding the trivial case
-    if(np.sum(vec_t) == 0): 
-        vec_t[random.randint(0, vec_t.size-1)] = 1
-    
-    # Probability Testing
-    if(prob(ob(vec_i, objective_matrix), ob(vec_t, objective_matrix), i) >= random.random()):
-        vec_i = vec_t.copy()
+    # Matrices/Vectors
+    vec_i = np.full((objective_matrix.shape[0]),0, dtype=bool)
+    vec_i[random.randint(0, vec_i.size-1)] = 1
+
+    # Iteration
+    for i in temps:
+        vec_t = vec_i.copy() 
+        
+        # Toggling a random bit
+        random_index = random.randint(0, vec_i.size-1)
+        vec_t[random_index] = not vec_t[random_index]
+        
+        # Avoiding the trivial case
+        if(np.sum(vec_t) == 0): 
+            vec_t[random.randint(0, vec_t.size-1)] = 1
+        
+        # Probability Testing
+        if(prob(ob(vec_i, objective_matrix), ob(vec_t, objective_matrix), i) >= random.random()):
+            vec_i = vec_t.copy()
+    return vec_i
