5a7154a88dff38c24a164143936c7fe87db9ff5f
Pixelqted <pixelatedwastaken@gmail.com>
2024-09-12 23:49:30 -0400

    Logarithmic annealing schedule implemented

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,38 +1,36 @@
+# Libraries
 import numpy as np 
 import random
 import math
 import matplotlib.pyplot as plt
+# Functions
 def ob(vec, matrix): 
     if(matrix.shape[0] != vec.size): return
-    # print(np.dot(vec, np.dot(matrix, vec.T)))
-    return np.dot(vec, np.dot(matrix, vec.T))
-
-start_temp = 1000
-iterations = 100000
-temps = np.linspace(start_temp, 1, iterations)
+    return np.dot(vec, np.dot(matrix, vec))
 
 def prob(old, new, temp): 
-    return math.exp((old-new)/temp)
+    return math.exp(-(new-old)/(temp))
 
-# objective_matrix = np.random.random((10,10))
-objective_matrix = random.random()*np.random.randint( -3, 4, (10,10))
-initial_vector = np.empty((objective_matrix.shape[0]))
-initial_vector[0] = 1
-final_vector = initial_vector
-values = []
+# Parameters
+iterations = 100000
+_ = np.vectorize(lambda x: -100*math.log(x) + math.log(iterations)*100)
+temps = _(np.linspace(1, iterations-1, iterations-1))
+objective_matrix = np.triu(np.random.random((2,2)), k=0)
+vec_i = np.full((objective_matrix.shape[0]),0, dtype=bool)
+vec_i[random.randint(0, vec_i.size-1)] = 1
+
+# Iteration
 for i in temps:
-    intermediate_vector = final_vector.copy() 
-    # Turning a random bit on and a random bit off
-    intermediate_vector[random.randint(0, initial_vector.shape[0]-1)] = 1
-    intermediate_vector[random.randint(0, initial_vector.shape[0]-1)] = 0
-    # print(ob(intermediate_vector, objective_matrix))
-    values.append(ob(intermediate_vector, objective_matrix))
-    if prob(ob(intermediate_vector, objective_matrix), ob(final_vector, objective_matrix), i) < random.random(): 
-        final_vector = intermediate_vector
-
-print(final_vector)
-print(objective_matrix)
-print(ob(final_vector, objective_matrix))
-x = np.linspace(1, iterations, iterations)
-plt.plot(x, values)
-plt.show()
+    vec_t = vec_i.copy() 
+    
+    # Toggling a random bit
+    random_index = random.randint(0, vec_i.size-1)
+    vec_t[random_index] = not vec_t[random_index]
+    
+    # Avoiding the trivial case
+    if(np.sum(vec_t) == 0): 
+        vec_t[random.randint(0, vec_t.size-1)] = 1
+    
+    # Probability Testing
+    if(prob(ob(vec_i, objective_matrix), ob(vec_t, objective_matrix), i) >= random.random()):
+        vec_i = vec_t.copy()
