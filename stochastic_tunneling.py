e2ffde4d82cb901d99f1d91e727ab223ed52efba
Pixelqted <pixelatedwastaken@gmail.com>
2024-09-14 16:04:34 -0400

    stochastic annealing

--- stochastic_tunneling.py
+++ stochastic_tunneling.py
@@ -1,35 +1,39 @@
-# Libraries
 import numpy as np 
-import random
+import random 
 import math
-import matplotlib.pyplot as plt
-# import the required functions 
-from functions import ob,prob,transform
+from functions import ob, prob, transform
 
-def stochastic_tunnel_optimize(iterations, objective_matrix): 
+def adaptive_anneal(iterations, matrix):
     # Parameters
-    _ = np.vectorize(lambda x: -100*math.log(x) + math.log(iterations)*100)
-    temps = 100*(-np.log(np.linspace(1, iterations-1, iterations-1)) + math.log(iterations))
-    min = 0
-    
-    # Matrices/Vectors
-    vec_i = np.full((objective_matrix.shape[0]),0, dtype=bool)
-    vec_i[random.randint(0, vec_i.size-1)] = 1
-    
-    # Iteration
-    for i in temps:
-        vec_t = vec_i.copy() 
+    temps = 10*(-np.log(np.linspace(1, iterations-1, iterations-1)) + math.log(iterations))
+    # The initial bit
+    initial_bit = np.zeros(matrix.shape[0], dtype=bool)
+    initial_bit[random.randint(0, matrix.shape[0] -1)] = 1
+    min_val = 0
+    # Analytics
+    funcs = []
+    mins = []
+    for i in temps: 
+        # Creating a new candidate 
+        random_index = random.randint(0, matrix.shape[0] -1)
+        new_bit = initial_bit.copy()
+        new_bit[random_index] = not new_bit[random_index]
         
-        # Toggling a random bit
-        random_index = random.randint(0, vec_i.size-1)
-        vec_t[random_index] = not vec_t[random_index]
+        # Avoiding the trivial case 
+        if(np.sum(new_bit) == 0): 
+            random_index = random.randint(0, matrix.shape[0] -1)
+            new_bit[random_index] = not new_bit[random_index]
         
-        # Avoiding the trivial case
-        if(np.sum(vec_t) == 0): 
-            vec_t[random.randint(0, vec_t.size-1)] = 1
-        
-        # Probability Testing
-        if(prob(transform(vec_i, objective_matrix, min), transform(vec_t, objective_matrix, min), i) >= random.random()):
-            vec_i = vec_t.copy()
-            min = ob(vec_i, objective_matrix)
-    return vec_i
+        # Testing the probabilities of the new versus old case 
+        old_energy = transform(initial_bit, matrix, min_val, len(mins)+1)
+        new_energy = transform(new_bit, matrix, min_val, len(mins)+1)
+        new_ob = ob(new_bit, matrix)
+        # funcs.append(new_ob)
+        if(prob(old_energy, new_energy, i) >= random.random()): 
+            if(i == temps[0] or new_ob < min_val):
+                mins.append(new_ob)
+                min_val = np.min(mins)
+            initial_bit = new_bit
+       
+    return initial_bit, temps, funcs, mins
+
