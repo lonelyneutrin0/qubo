53629454e3745364d93c4c5e236cccf045899546
Pixelqted <pixelatedwastaken@gmail.com>
2024-10-27 17:00:38 -0400

    stochastic tunneling rewrite- OOP, docstrings, comments

--- SA_example.py
+++ SA_example.py
@@ -1,21 +1,36 @@
-from simulated_annealing import Annealer
+from stochastic_tunneling import Annealer as st_annealer
+from simulated_annealing import Annealer as sa_annealer
 import numpy as np
 import matplotlib.pyplot as plt
-x = Annealer(iterations=1000, ml=100, T_0=10, T_f=1.0)
+st = st_annealer(iterations=1000, ml=100, T_0=10, T_f=1.0)
+sa = sa_annealer(iterations=1000, ml=100, T_0=10, T_f=1.0)
 
 Q = -np.random.uniform(low=0, high=10, size=(3,3))
-sol = x.anneal(Q=Q)
-x_min = sol.x_min
-print(x_min.T @ Q @ x_min, x_min)
-plt.plot(np.arange(1000), sol.accepts) 
+st_sol = st.anneal(Q=Q)
+sa_sol = sa.anneal(Q=Q)
+st_x_min = st_sol.x_min
+sa_x_min = sa_sol.x_min
+
+print(f'Simulated Annealing Minimum: {sa_x_min.T @ Q @ sa_x_min}')
+print(f'Stochasic Tuneneling Minimum: {st_x_min.T @ Q @ st_x_min}')
+
+plt.plot(np.arange(1000), st_sol.accepts, label='STSA')
+plt.plot(np.arange(1000), sa_sol.accepts, label='SA') 
 plt.xlabel("Iteration No.")
 plt.ylabel("Cumalative Acceptance No.")
+plt.legend()
 plt.savefig('acceptances.png', dpi=800, bbox_inches='tight')
 plt.close()
-plt.plot(np.arange(1000), sol.energies)
+
+plt.plot(np.arange(1000), st_sol.energies, label='STSA')
+plt.plot(np.arange(1000), sa_sol.energies, label='SA')
 plt.ylabel("Energy")
+plt.legend()
 plt.savefig('energies.png', dpi=800, bbox_inches='tight')
 plt.close() 
-plt.plot(np.arange(1000), sol.rejects)
+
+plt.plot(np.arange(1000), st_sol.rejects, label='STSA')
+plt.plot(np.arange(1000), sa_sol.rejects, label='SA')
 plt.ylabel("Cumalative Rejection No.")
+plt.legend()
 plt.savefig('rejections.png', dpi=800, bbox_inches='tight')
--- stochastic_tunneling.py
+++ stochastic_tunneling.py
@@ -1,39 +1,106 @@
-import numpy as np 
-import random 
-import math
-from functions import ob, prob, transform
+import numpy as np
+from dataclasses import dataclass
+from numpy import ndarray
+from numpy.typing import ArrayLike
+@dataclass
+class solution: 
+    x_min: ndarray 
+    xs: ndarray
+    energies: ArrayLike 
+    accepts: ArrayLike
+    rejects: ArrayLike
+    """ 
+    A container class for outputting the optimal solution to the given QUBO problem
+    :param x_min: The bit vector corresponding to the minimum of the QUBO
+    :param xs: The various bit vectors cycled through throughout the annealing process
+    :param energies: An energy series for data analysis
+    :param accepts: The cumalative number of accepts at each iteration
+    :param rejects: The cumalative number of rejects at each iteration 
+    """
+@dataclass
+class Annealer:
+     ml: int 
+     iterations: int
+     T_0: float 
+     T_f: float 
+     X:ndarray=None
 
-def adaptive_anneal(iterations, matrix):
-    # Parameters
-    temps = 10*(-np.log(np.linspace(1, iterations-1, iterations-1)) + math.log(iterations))
-    # The initial bit
-    initial_bit = np.zeros(matrix.shape[0], dtype=bool)
-    initial_bit[random.randint(0, matrix.shape[0] -1)] = 1
-    min_val = 0
-    # Analytics
-    funcs = []
-    mins = []
-    for i in temps: 
-        # Creating a new candidate 
-        random_index = random.randint(0, matrix.shape[0] -1)
-        new_bit = initial_bit.copy()
-        new_bit[random_index] = not new_bit[random_index]
+     """" 
+     A container class storing the hyperparameters of the optimization algorithm 
+
+     :param ml: The Markov Chain length for each random walk 
+     :param iterations: The number of iterations for the annealing process 
+     :param T_0: The starting temperature 
+     :param T_f: The final temperature 
+     :param X: The initial bit array. Defaults to None
+     """
+
+     def anneal(self, Q:ndarray)->solution: 
+          """ 
+          The main annealing function 
+          :param Q: The QUBO matrix to optimize 
+          :returns: A solution object containing run information 
+          :raises ValueError: if the QUBO is not a square matrix or the initial bit vector is of inappropriate dimensionality 
+          """
+          
+          # Check the given matrix
+          if(len(Q.shape) != 2 or Q.shape[0] != Q.shape[1]): raise ValueError("Ensure that the QUBO is of proper dimensionality.")
+          
+          # Check the bit vector
+          if(self.X is not None and self.X.shape[0] != Q.shape[0]): raise ValueError("Make sure the initial bit vector is of appropriate dimensionality")
+          
+          # If the initial bit vector is None, generate a random initial solution 
+          self.X = np.random.randint(low=0, high=2, size=(Q.shape[0]), dtype=bool) if self.X is None else self.X
+          
+          # Initialize the ndarrays for data analytics, temperature and the random numbers to be used 
+          random_numbers = np.random.uniform(size=(self.iterations, self.ml))
+          
+          # Energies, bit vectors, accepts and rejects are kept track of after every random walk 
+          energies = np.zeros((self.iterations))
+          bit_vectors = np.zeros((self.iterations, self.X.shape[0]))
+          accepts = np.zeros((self.iterations))
+          rejects = np.zeros((self.iterations))
+          temperatures = np.linspace(start=self.T_0, stop=self.T_f, num=self.iterations)
+          iter_energy = (self.X).T @ Q @ self.X 
+          candidate_energy = 0
+          # Initiate the annealing
+          for step in range(self.iterations): 
+               
+               # Log the current bit vector and energy value 
+               energies[step] = iter_energy
+               bit_vectors[step] = self.X 
+               iter_temp = temperatures[step]
+               
+               for ml_step in range(self.ml):
+                # Perturb a random bit of the bit vector 
+                ran_i = np.random.randint(low=0, high=self.X.shape[0])
+                temp_X = np.copy(self.X)
+                temp_X[ran_i] = not temp_X[ran_i]
+                
+                # Compute the energy of the new bit vector
+                temp_energy = temp_X.T @ Q @ temp_X
+                
+                # Stochastic Parameter Energy Difference
+                diff = np.exp(-step*(iter_energy-candidate_energy)*(10**-3)) - np.exp(-step*(temp_energy-candidate_energy)*(10**-3))
+                
+                # Analyze new states by the Metropolis condition
+                if( (diff) < 0 or np.exp(-(diff)/temperatures[step]) > random_numbers[step, ml_step]):
+                    self.X = temp_X
+                    iter_energy = temp_energy
+                    accepts[step] = 1
+                else: 
+                    rejects[step] = 1
+                if temp_energy < iter_energy: 
+                    candidate_energy = temp_energy
         
-        # Avoiding the trivial case 
-        if(np.sum(new_bit) == 0): 
-            random_index = random.randint(0, matrix.shape[0] -1)
-            new_bit[random_index] = not new_bit[random_index]
-        
-        # Testing the probabilities of the new versus old case 
-        old_energy = transform(initial_bit, matrix, min_val, len(mins)+1)
-        new_energy = transform(new_bit, matrix, min_val, len(mins)+1)
-        new_ob = ob(new_bit, matrix)
-        # funcs.append(new_ob)
-        if(prob(old_energy, new_energy, i) >= random.random()): 
-            if(i == temps[0] or new_ob < min_val):
-                mins.append(new_ob)
-                min_val = np.min(mins)
-            initial_bit = new_bit
-       
-    return initial_bit, temps, funcs, mins
+          # Compile the results 
+          sol = { 
+              'x_min': self.X, 
+              'xs': bit_vectors,
+              'energies': energies, 
+              'accepts': np.cumsum(accepts),
+              'rejects': np.cumsum(rejects)
+          }
+          return solution(**sol)
+
 
